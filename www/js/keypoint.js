6cac27430c9ca711a27b9da61be7c3c78955e73c
Mohamed Taman <mohamed.taman@gmail.com>
2018-10-25 09:52:25 -0700

    Oracle Code One 2018

--- www/js/keypoint.js
+++ www/js/keypoint.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
 
     var slides = $(".slide"),
         gallery,
@@ -11,18 +11,18 @@
         FastClick.attach(document.body);
     });
 
-    $("#wrapper").on('touchmove', function () {
+    $("#wrapper").on('touchmove', function() {
         return false;
     });
 
-    window.onkeyup = function (event) {
+    window.onkeyup = function(event) {
         if (event.keyCode === 39) {
             return gallery.next();
         }
         if (event.keyCode === 37) {
             return gallery.prev();
         }
-    }
+    };
 
     $("#menu-btn").on('click', toggleMenu);
 
@@ -32,19 +32,19 @@
         }
     });
 
-    window.onhashchange = function () {
+    window.onhashchange = function() {
         var p = parseInt(window.location.hash.substr(1)) || 0;
         gallery.goToPage(p);
-    }
+    };
 
     prettyPrint();
 
     $("#slides").remove();
 
     var liStr = "";
-    for (var i=0; i<slides.length; i++) {
-        var h1 = $("h1", slides[i]);
-        liStr += '<li class="topcoat-list__item"><a href="#' + i + '">' + (h1[0] ? h1[0].innerText : '&nbsp;') + "</a></li>";
+    for (var x = 0; x < slides.length; x++) {
+        var h1 = $("h1", slides[x]);
+        liStr += '<li class="topcoat-list__item"><a href="#' + x + '">' + (h1[0] ? h1[0].innerText : '&nbsp;') + "</a></li>";
     }
     $("#list").html(liStr);
 
@@ -56,7 +56,7 @@
             $menu.addClass("visible");
         }
         return false;
-    };
+    }
 
     gallery = new SwipeView('#wrapper', { numberOfPages: slides.length });
 
@@ -65,12 +65,12 @@
         gallery.masterPages[i].appendChild(slides[page]);
     }
 
-    setTimeout(function () {
+    setTimeout(function() {
         var p = parseInt(window.location.hash.substr(1)) || 0;
         gallery.goToPage(p);
     });
 
-    gallery.onFlip(function () {
+    gallery.onFlip(function() {
 
         var el,
             upcoming,
@@ -82,7 +82,8 @@
             upcoming = gallery.masterPages[i].dataset.upcomingPageIndex;
             if (upcoming != gallery.masterPages[i].dataset.pageIndex) {
                 el = gallery.masterPages[i].querySelector('.slide');
-                if (el) gallery.masterPages[i].removeChild(el);
+                if (el)
+                    gallery.masterPages[i].removeChild(el);
                 el = gallery.masterPages[i].appendChild(slides[upcoming]);
                 el.className += " loading";
             }
@@ -90,4 +91,4 @@
 
     });
 
-}())
+}());
